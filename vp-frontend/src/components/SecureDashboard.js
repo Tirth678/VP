2aa46bc7e61edd8a3982d45a6602e93c6ceea3a6
Bhargav <bhargav.umetiya@gmail.com>
2025-01-31 16:15:41 +0530

    Add files via upload

--- vp-frontend/src/components/SecureDashboard.js
+++ vp-frontend/src/components/SecureDashboard.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
+const Dashboard = () => {
+  const { t } = useTranslation();
+  
+  return (
+    <div className="dashboard">
+      <h1>{t('welcome')}</h1>
+      {/* Budgeting UI, Transaction History */}
+    </div>
+  );
+};
+
+export default Dashboard;
